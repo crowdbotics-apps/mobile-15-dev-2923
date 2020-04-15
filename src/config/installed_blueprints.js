a70da0941db305e86f8319d61ac2195e94720cb4
Crowdbotics <team@crowdbotics.com>
2020-04-15 07:16:20 +0000

    add package blueprint-react-native-profiles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'UserProfile2410', human_name: 'User Profile', access_route: 'UserProfile2410'},
 { name: 'EmailAuth2409', human_name: 'Email Auth', access_route: 'EmailAuth2409', icon: 'envelope-o'},
 { name: 'EmailAuth2407', human_name: 'Email Auth', access_route: 'EmailAuth2407', icon: 'envelope-o'},
 { name: 'UserProfile2406', human_name: 'User Profile', access_route: 'UserProfile2406'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import UserProfile2410Navigator from '../features/UserProfile2410/navigator';
 import EmailAuth2409Navigator from '../features/EmailAuth2409/navigator';
 import UserProfile2406Navigator from '../features/UserProfile2406/navigator';
 import UserProfile2405Navigator from '../features/UserProfile2405/navigator';
@@ -19,6 +20,7 @@ const AppNavigator = {
       screen: SplashScreen
     },
     //@BlueprintNavigationInsertion
+UserProfile2410: { screen: UserProfile2410Navigator },
 EmailAuth2409: { screen: EmailAuth2409Navigator },
 UserProfile2406: { screen: UserProfile2406Navigator },
 UserProfile2405: { screen: UserProfile2405Navigator },
