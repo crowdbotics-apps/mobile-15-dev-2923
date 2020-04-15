7f6236747f6a515b636953d349124862d54c5f4d
Crowdbotics <team@crowdbotics.com>
2020-04-15 07:15:31 +0000

    add package blueprint-react-native-emailauth-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import EmailAuth2409Navigator from '../features/EmailAuth2409/navigator';
 import UserProfile2406Navigator from '../features/UserProfile2406/navigator';
 import UserProfile2405Navigator from '../features/UserProfile2405/navigator';
 import EmailAuthNavigator from '../features/EmailAuth/navigator';
@@ -18,6 +19,7 @@ const AppNavigator = {
       screen: SplashScreen
     },
     //@BlueprintNavigationInsertion
+EmailAuth2409: { screen: EmailAuth2409Navigator },
 UserProfile2406: { screen: UserProfile2406Navigator },
 UserProfile2405: { screen: UserProfile2405Navigator },
 EmailAuth: { screen: EmailAuthNavigator },
